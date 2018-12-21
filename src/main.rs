85dabd7c1a3e46f1e8f6490ca3a087b4c0bf9b06
Louis FRADIN <louis.fradin@gmail.com>
2018-12-21 23:32:16 +0100

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut untouched_directory = String::new();
+    let mut pruned_directory = String::new();
+
+    {
+        let mut ap = ArgumentParser::new();
+        ap.refer(&mut untouched_directory)
+            .add_argument(&"untouched_directory", Store,
+            "Untouched directory");
+        ap.refer(&mut pruned_directory)
+            .add_argument(&"pruned_directory", Store,
+            "Directory to prune");
+        ap.parse_args_or_exit();
+        ap.set_description("This utilitary will find redundant files between two directories (or not).");
+    }
+
+    println!("Hello, world! {} {}", untouched_directory, pruned_directory);
 }
